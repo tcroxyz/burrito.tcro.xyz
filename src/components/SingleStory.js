11bf0aa0d520a6fb5ede2a126a0b17cc80451f8c
tacobeanzz <thisistacotaco@gmail.com>
2022-03-10 23:07:56 +0000

    design changed

--- src/components/SingleStory.js
+++ src/components/SingleStory.js
@@ -36,12 +36,12 @@ export default function SinglePost() {
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
       exit={{ opacity: 0, duration: 2 }}
-      className="bg-purple-100 min-h-screen p-12"
+      className="bg-purple-100 min-h-screen p-4 pt-6 lg:p-16"
     >
       <article className="container shadow-lg mx-auto bg-purple-200 rounded-lg border-2 border-gray-100">
         <header className="relative">
           <div className="absolute h-full w-full flex items-center justify-center p-8">
-            <div className="bg-purple-300 bg-opacity-75 rounded p-12 mt-10 shadow-lg border-2 border-gray-100">
+            <div className="bg-purple-300 bg-opacity-75 rounded p-12 mt-12 shadow-lg border-2 border-gray-100">
               <h1 className="cursive text-3xl lg:text-6xl mb-4">
                 {singlePost.title}
               </h1>
@@ -51,7 +51,7 @@ export default function SinglePost() {
             </div>
           </div>
         </header>
-        <div className="px-6 lg:px-20 py-40 lg:py-40 prose lg:prose-xl max-w-full  rounded-md ">
+        <div className="px-4 lg:px-20 py-40 lg:py-40 prose lg:prose-xl max-w-full rounded-md ">
           <BlockConent
             blocks={singlePost.body}
             projectId="b1p89kdm"
